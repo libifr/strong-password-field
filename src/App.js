26084476b15dff927161f0b3b9ba454ad726bd5a
Libi Avidan <libifridman123@gmail.com>
2022-12-13 14:35:35 +0200

    password-field-init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react'
 
 function App() {
+  const [password, setPassword] = useState("")
+  const [allowSubmit, setAllowSubmit] = useState(false)
+  const [isStrongPassword, setIsStrongPassword] = useState("weak")
+
+  useEffect(()=>{
+    if(password.length < 3) {
+      setIsStrongPassword("Weak")
+      setAllowSubmit(false)
+    }
+    if(password.length >= 3 && password.length < 7) {
+      setIsStrongPassword("Medium")
+      setAllowSubmit(true)
+
+    }
+    if(password.length > 7) {
+      setIsStrongPassword("Strong")
+      setAllowSubmit(true)
+    }
+
+  },[isStrongPassword,password,allowSubmit])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <label>Enter Your Password:</label>
+        <input type="text" onChange={(e)=>{setPassword(e.target.value)}}></input>
+        <input type="submit" disabled={allowSubmit ? false : true}></input>
+        <p>Your Password is {isStrongPassword}</p>
     </div>
   );
 }
